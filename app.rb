52b3af308d18e4a61da148983fafee68657c5e6f
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-21 21:30:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
 
@@ -14,5 +15,14 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do 
+    @user_name = params[:name]
+    "Goodbye #{@user_name}."
+  end
+  
+  get "/multiply/:num1/:num2" do 
+    @number_1 = params[:num1]
+    @number_2 = params[:num2]
+    binding.pry
 
 end
