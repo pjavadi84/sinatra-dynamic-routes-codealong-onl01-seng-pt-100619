245aed783781ec5493813abf2a0c97570cd354a9
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-21 21:39:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do 
-    number_1 = params[:num1]
+    number_1 = params[:num1].to_i
     binding.pry
     number_2 = params[:num2]
     # @total = number_1 * number_2
