2fc6dfc2915b321109dba824f147023c522756c8
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-21 21:33:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
   get "/multiply/:num1/:num2" do 
     @number_1 = params[:num1]
     @number_2 = params[:num2]
-    "#{@number_1 * @number_2}"
+
     binding.pry
   end
 
